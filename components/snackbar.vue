56465df100422ea990e6238302b6881d0bde29fb
10Shubham01 <shubham.gupta.17.1@mountblue.tech>
2021-11-08 17:16:10 +0530

    snackbar position changed to top

--- components/snackbar.vue
+++ components/snackbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-snackbar v-model="snackbar" :timeout="timeout">
+  <v-snackbar v-model="snackbar" :timeout="timeout" absolute top>
     {{ text }}
     <template v-slot:action="{ attrs }">
       <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
